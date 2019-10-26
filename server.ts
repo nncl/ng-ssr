23e9c365d931344abde02ed46f5c1b2ccad73853
= <nncl@live.com>
2019-10-26 14:21:17 -0300

    Build configurations

--- server.ts
+++ server.ts
@@ -18,13 +18,15 @@
 import 'zone.js/dist/zone-node';
 
 import * as express from 'express';
-import {join} from 'path';
+import { join } from 'path';
 
 // Express server
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
+
+const detector = require('spider-detector');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
@@ -40,6 +42,8 @@ app.engine('html', ngExpressEngine({
 app.set('view engine', 'html');
 app.set('views', DIST_FOLDER);
 
+app.use(detector.middleware());
+
 // Example Express Rest API endpoints
 // app.get('/api/**', (req, res) => { });
 // Serve static files from /browser
@@ -49,7 +53,11 @@ app.get('*.*', express.static(DIST_FOLDER, {
 
 // All regular routes use the Universal engine
 app.get('*', (req, res) => {
-  res.render('index', { req });
+  if (req.isSpider()) {
+    res.render(join(DIST_FOLDER, 'ssr', 'index'), { req });
+  } else {
+    res.sendFile(join(DIST_FOLDER, 'csr', 'index'));
+  }
 });
 
 // Start up the Node server
